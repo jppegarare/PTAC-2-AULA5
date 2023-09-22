4a14fdfba9ed4d9b15d4492bbfa417168184c116
João Pegarare <jppegarare18@gmail.com>
2023-09-22 00:08:31 -0400

    percebi que estava fazendo a atividade no local errado e troquei o código de arquivo

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,52 +1,5 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 export default function Home() {
-    const[personagem, setPersongem] = useState("")
-    const[idade, setIdade] = useState(1)
-    const[recompensa, setRecompensa] = useState("")
-    const[listas, setListas] = useState([])
-    const[identidade, setIdentidade] = useState(1)
 
-    const salvar = (e) => {
-        e.preventDefault()
-        setListas([...listas, {
-            personagem:personagem, idade:idade, recompensa:recompensa, identidade:identidade
-        }])
-        setIdentidade(identidade + 1)
-        console.log(listas)
-    }
-   
-    const remover  = (id) => {
-        const auxLista = [];
-        listas.map((lista) => {
-            if (lista.id !== id){
-                auxLista.push(lista);
-            }
-        })
-        setListas(auxLista)
-    }
-
-    return (
-        <div>
-            <Link to="/todo">todo</Link>
-            <h1>Lista de Atividades</h1>
-
-            <form onSubmit={salvar}>
-                <input type="text" onChange={(e) => {setPersongem(e.target.value)}}></input>
-                <input type="number" onChange={(e) => {setIdade(e.target.value)}}></input>
-                <input type="text" onChange={(e) => {setRecompensa(e.target.value)}}></input>
-                <button>Salvar</button>
-            </form>
-
-            {listas.map((atv) => 
-                <div key = {atv.identidade}>
-                    <p>{atv.personagem}</p>
-                    <p>{atv.idade}</p>
-                    <p>{atv.recompensa}</p>
-                    <button onClick={() => remover(atv.id)}>Remover</button>                           
-                </div>
-            )}
-        </div>
-
-    );
 }
--- src/Todo/ToDo.jsx
+++ src/Todo/ToDo.jsx
@@ -2,12 +2,61 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function ToDo() {
+    const[personagem, setPersongem] = useState("")
+    const[idade, setIdade] = useState(1)
+    const[recompensa, setRecompensa] = useState("")
+    const[listas, setListas] = useState([])
+    const[identidade, setIdentidade] = useState(1)
+
+    const salvar = (e) => {
+        e.preventDefault()
+        setListas([...listas, {
+            personagem:personagem, idade:idade, recompensa:recompensa, identidade:identidade
+        }])
+        setIdentidade(identidade + 1)
+        console.log(listas)
+    }
    
+    const remover  = (id) => {
+        const auxLista = [];
+        listas.map((lista) => {
+            if (lista.id !== id){
+                auxLista.push(lista);
+            }
+        })
+        setListas(auxLista)
+    }
+
     return (
         <div>
-            <h1>Lista de Atividades</h1>
-            <Link to="/">home</Link>
-           
+            <Link to="/home">Home</Link>
+            <h1>Informe o Foragido </h1>
+
+            <form onSubmit={salvar}>
+                <h3>Nome</h3>
+                <input type="text" onChange={(e) => {setPersongem(e.target.value)}}></input>
+
+                <h3>Idade</h3>
+                <input type="number" onChange={(e) => {setIdade(e.target.value)}}></input>
+
+                <h3>Recompensa</h3>
+                <input type="text" onChange={(e) => {setRecompensa(e.target.value)}}></input>
+
+                <br/>
+                <br/>
+
+                <button>Salvar</button>
+            </form>
+
+            {listas.map((atv) => 
+                <div key = {atv.identidade}>
+                    <p>{atv.personagem}</p>
+                    <p>{atv.idade}</p>
+                    <p>{atv.recompensa}</p>
+                    <button onClick={() => remover(atv.id)}>Remover</button>                           
+                </div>
+            )}
         </div>
+
     );
 }
