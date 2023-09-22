2f14f255a2ad542c37dc38d0989f29bccc2b4544
João Pegarare <jppegarare18@gmail.com>
2023-09-22 00:11:44 -0400

    criando o link do ToDo no Home

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,5 +1,10 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 export default function Home() {
-
+    return(
+        <div>
+            <h1>Informe o Suejeito no Link abaixo</h1>
+            <Link to="/todo">todo</Link>
+        </div>
+    )
 }
